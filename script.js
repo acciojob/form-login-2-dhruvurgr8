a87793acf66fc8126caf0655cf7d4cc37dcd0ac6
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-02-14 17:38:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const form = document.querySelector('.form');
+form.addEventListener('submit',(e)=>{
+	e.preventDefault();
+	const fname = form.firstname.value;
+	const lname = form.lastname.value;
+	const number = form.number.value;
+	const email = form.email.value;
+	alert(`first Name: ${fname} Last Name: ${lname} Number: ${number} Email ID: ${email}`)
+	
+})
